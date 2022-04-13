1a65d8d297588ae5732957449ff82a32216cb586
Felipe Santos Poch <lipepock@gmail.com>
2022-04-13 12:00:50 +0200

    Add files via upload

---
+++ C01/sandbox/C01_eval-master/C01_eval-master/ex08/ft_sort_int_tab.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: felsanto <felsanto@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/10 14:14:45 by felsanto          #+#    #+#             */
+/*   Updated: 2022/04/11 20:41:11 by felsanto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	i;
+	int	j;
+	int	temp;
+
+	i = 0;
+	while (i < size)
+	{
+		j = 0;
+		while (j < size - i -1)
+		{
+			if (tab[j] > tab[j + 1])
+			{
+				temp = tab[j];
+				tab[j] = tab[j + 1];
+				tab[j + 1] = temp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
