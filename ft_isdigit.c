5de839eff97a5240f03b76fdea6ac0ecc6dc0565
Felipe Santos <fpedruski@icloud.com>
2022-05-09 17:27:59 +0200

    Adding isdigit

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -6,17 +6,25 @@
 /*   By: felsanto <felsanto@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 16:57:27 by felsanto          #+#    #+#             */
-/*   Updated: 2022/05/09 17:06:55 by felsanto         ###   ########.fr       */
+/*   Updated: 2022/05/09 17:27:33 by felsanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <stdio.h>
 
-int ft_isdigit(int i)
+
+int ft_isdigit(int *i)
 {
-	while(str[] != '\0')
+	if(*i > 0 && *i < 128 )
+	{
+		printf("%d", *i);
+		return (1);
+	}
+	return (0);
 }
-
 
 int main()
 {
-	ft_isdigit(123);	
+	int str;
+	str = '1';
+	ft_isdigit(&str);	
 }
