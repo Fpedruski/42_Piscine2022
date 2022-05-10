44d40a802f641446fa0ae9c973f985709a5639bd
Felipe Santos <fpedruski@icloud.com>
2022-05-10 17:02:57 +0200

    Adding in main

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -6,7 +6,7 @@
 /*   By: felsanto <felsanto@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 16:57:27 by felsanto          #+#    #+#             */
-/*   Updated: 2022/05/10 12:59:25 by felsanto         ###   ########.fr       */
+/*   Updated: 2022/05/10 16:58:42 by felsanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
@@ -17,38 +17,4 @@ int ft_isdigit(int i)
 	
 	return(i >= 48 && i <= 57);
 	
-}
-
-
-int main()
-{
-	int var1 = 'H';
-	int var2 = '1';
-
-	// isdigit function
-	printf("This is how isdigit function is:\n");
-	if(isdigit(var1))
-		printf("Var1 Is a Digit: %d\n", var1);
-	else 
-		printf("Var1 Not a Digit: %c\n", var1);
-	
-	if(isdigit(var2))
-		printf("Var2 Is a Digit: %d\n", var2);
-	else
-		printf("Var2 Not a Digit: %c\n", var2);
-	
-	printf("\n\n");
-	// Main function
-	printf("This is your function:\n");
-	if(ft_isdigit(var1))
-		printf("Var1 Is a Digit:%d\n", var1);	
-	else
-		printf("Var1 Not a Digit: %c\n", var1);
-
-	if(ft_isdigit(var2))
-		printf("Var2 Is a Digit: %d\n", var2);
-	else
-		printf("Var2 Not a Digiti: %c\n", var2);
-
-	return 0;
 }
--- main.c
+++ main.c
@@ -6,13 +6,15 @@
 /*   By: felsanto <felsanto@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/10 16:42:31 by felsanto          #+#    #+#             */
-/*   Updated: 2022/05/10 16:43:51 by felsanto         ###   ########.fr       */
+/*   Updated: 2022/05/10 17:02:47 by felsanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 # include "libft.h"
 
 int	main()
 {
+
+	/* IS_ALPHA */
 	int var1 = '1';
 	int var2 = 'A';
 	int var3 = 'a';
@@ -22,5 +24,40 @@ int	main()
 	printf("%c return %d |", var2, ft_isalpha(var2));
 	printf("%c return %d |", var3, ft_isalpha(var3));
 	printf("%c return %d \n", var4, ft_isalpha(var4));
+	
+	/*IS_DIGIT*/
+
+	int var1 = 'H';
+	int var2 = '1';
+
+	// isdigit function
+	printf("This is how isdigit function is:\n");
+	if(isdigit(var1))
+		printf("Var1 Is a Digit: %d\n", var1);
+	else 
+		printf("Var1 Not a Digit: %c\n", var1);
+	
+	if(isdigit(var2))
+		printf("Var2 Is a Digit: %d\n", var2);
+	else
+		printf("Var2 Not a Digit: %c\n", var2);
+	
+	printf("\n\n");
+	// Main function
+	printf("This is your function:\n");
+	if(ft_isdigit(var1))
+		printf("Var1 Is a Digit:%d\n", var1);	
+	else
+		printf("Var1 Not a Digit: %c\n", var1);
+
+	if(ft_isdigit(var2))
+		printf("Var2 Is a Digit: %d\n", var2);
+	else
+		printf("Var2 Not a Digiti: %c\n", var2);
+
+	return 0;	
+
+	/*IS_ALPHA*/
+	
 	return (0);
 }
