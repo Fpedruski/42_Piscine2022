531ef5c133420105d0a7071329e19c10c326e36f
fpedruski <fpedruski@icloud.com>
2022-05-10 09:08:26 +0200

    Test Libft

--- Libft/ft_isdigit.c
+++ Libft/ft_isdigit.c
@@ -9,14 +9,46 @@
 /*   Updated: 2022/05/09 17:06:55 by felsanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <stdio.h>
+#include <ctype.h>
 
 int ft_isdigit(int i)
 {
-	while(str[] != '\0')
+	
+	return(i >= 48 && i <= 57);
+	
 }
 
 
 int main()
 {
-	ft_isdigit(123);	
+	int var1 = 'H';
+	int var2 = '1';
+
+	// isdigit function
+	printf("This is how isdigit function is:\n");
+	if(isdigit(var1))
+		printf("Var1 Is a Digit: %d\n", var1);
+	else 
+		printf("Var1 Not a Digit: %c\n", var1);
+	
+	if(isdigit(var2))
+		printf("Var2 Is a Digit: %d\n", var2);
+	else
+		printf("Var2 Not a Digit: %c\n", var2);
+	
+	printf("\n\n");
+	// Main function
+	printf("This is your function:\n");
+	if(ft_isdigit(var1))
+		printf("Var1 Is a Digit:%d\n", var1);	
+	else
+		printf("Var1 Not a Digit: %c\n", var1);
+
+	if(ft_isdigit(var2))
+		printf("Var2 Is a Digit: %d\n", var2);
+	else
+		printf("Var2 Not a Digiti: %c\n", var2);
+
+	return 0;
 }
