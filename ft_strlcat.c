4acb8af571c78a3ee09b8f5b1b3214df64da641a
Felipe Santos <fpedruski@icloud.com>
2022-05-19 20:56:15 +0200

    Strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,8 +6,40 @@
 /*   By: felsanto <felsanto@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 16:43:48 by felsanto          #+#    #+#             */
-/*   Updated: 2022/05/19 17:56:24 by felsanto         ###   ########.fr       */
+/*   Updated: 2022/05/19 20:56:04 by felsanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
+size_t ft_strlcat(char *dst, char *src, size_t size)
+{
+	char	*s;
+	size_t	len_dst;
+	size_t	res;
+	size_t	len_src;
+	size_t	i;
+
+	s = (char *)src;
+	len_dst = ft_strlen(dst);
+	len_src = ft_strlen(src);
+	res = 0;
+	i = 0;
+	
+	if (size > len_dst)
+		res = len_src + len_dst;
+	else
+		res = len_src + size;
+	while (s[i] && (len_dst + 1) < size)
+	{
+		dst[len_dst] = s[i];
+		len_dst++;
+		i++;
+	}
+	dst[len_dst] = '\0';
+	return (res);
+}
+
+int main()
+{
+	
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: felsanto <felsanto@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 17:07:09 by felsanto          #+#    #+#             */
-/*   Updated: 2022/05/19 17:38:48 by felsanto         ###   ########.fr       */
+/*   Updated: 2022/05/19 20:15:58 by felsanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n);
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlen(const char *ch);
-size_t	ft_strlcat(char *dst, char *src, size_t dstsize);
+size_t	ft_strlcat(char *dst, char *src, size_t size); //CHECK 
 
 char	*ft_strchr(const char *str, int ch);
 char	*ft_strrchr(const char *str, int ch);
@@ -41,6 +41,6 @@ void	ft_bzero(void *str, size_t n);
 void	*ft_memset(void *dest, int ch, size_t i);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memchr(const void *src, int ch, size_t count);
-void	*ft_memmove(void *dest, const void *src, size_t len);
+void	*ft_memmove(void *dest, const void *src, size_t len); //CHECK
 
 #endif
