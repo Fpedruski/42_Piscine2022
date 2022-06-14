ab894b8703ef06eba12d49beca7d4e1fc4242baa
Felipe Santos <fpedruski@icloud.com>
2022-06-14 13:22:15 +0200

    Resolving strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,10 +6,11 @@
 /*   By: felsanto <felsanto@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/02 16:24:23 by felsanto          #+#    #+#             */
-/*   Updated: 2022/06/13 19:06:12 by felsanto         ###   ########.fr       */
+/*   Updated: 2022/06/14 13:16:48 by felsanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
+#include <stdio.h>
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
@@ -20,21 +21,30 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	string1 = (char *)s1;
 	string2 = (char *)s2;
-	if (!s1 || !s2 || n < 0)
-		return (-1);
-	while (i < n)
+	if ((s1[0] == '\0' && s2[0] == '\0') || n == 0)
+		return (0);
+	else if (strlen(s1) == strlen(s2))
 	{
-		if (*string1++ == *string2++)
-			return (0);
-		else
-			return (-1);
+		while (i < n)
+		{
+			if (*string1++ < *string2++)
+				return (-1);
+			else if (*string1++ == *string2++)
+				return (0);
+			else
+				return (1);
+		}
 	}
+	else if (strlen(s1) > strlen(s2) && n > 0)
+		return (1);
+	else if (strlen(s2) > strlen(s1) && n > 0)
+		return (-1);
 	return (0);
 }
 /*
 	int
 	main()
 	{
-		ft_strncmp("Hello", "There", 5);
+		printf("%d", ft_strncmp("", "There", 5));
 	}
 */
